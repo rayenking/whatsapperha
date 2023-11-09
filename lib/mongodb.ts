f192954e9e4b277cd22f0327930087d68e961cc5
rayenking <donttouch353@gmail.com>
2023-11-09 19:04:29 +0800

    feat: unused function

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,18 +4,13 @@ import { Db, MongoClient, MongoOptions } from 'mongodb';
 class RHDatabase {
     private url: string;
     private client: MongoClient;
-    private database: any;
     options: MongoOptions | undefined;
-    collection: any;
 
     constructor(){
         this.url = process.env.MONGODB_URL || ''
         console.log(this.url)
         this.client = new MongoClient(this.url);
         this.connect()
-
-        this.database = (dbname: string) => this.useDatabase(dbname)
-        this.collection = (collection: any) => this.database.collection(collection)
     }
 
     async connect(){
